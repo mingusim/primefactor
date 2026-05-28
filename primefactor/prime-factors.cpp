864f9dda1e114d47189e981bf789eaca9c0ccba7
mingu.sim <simmg0202@naver.com>
2026-05-28 09:41:03 +0900

    [feature 7] Step 7

--- primefactor/prime-factors.cpp
+++ primefactor/prime-factors.cpp
@@ -8,24 +8,14 @@ public:
 	vector<int> of(int number) {
 		vector<int> result = {};
 		int divisor = 2;
-		if (number > 1) {
-			if (number == 4 || number == 6 || number == 9) {
-				for (divisor = 2; number > 1; divisor++)
-				{
-					while (number % divisor == 0)
-					{
-						result.push_back(divisor);
-						number /= divisor;
-					}
-				}
+		for (divisor = 2; number > 1; divisor++)
+		{
+			while (number % divisor == 0)
+			{
+				result.push_back(divisor);
+				number /= divisor;
 			}
-
-			else {
-				result.push_back(number);
-			}
-
 		}
-
 		return result;
 	}
 };
